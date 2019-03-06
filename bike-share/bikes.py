74f3e87a7456eef727813494d5c5f3571163d725
Krishna Vedala <krishna.vedala@ieee.org>
2019-03-06 07:57:40 -0500

    added get and put parser options

--- bike-share/bikes.py
+++ bike-share/bikes.py
@@ -11,6 +11,19 @@ bike_fields = {
 
 BIKES = pd.DataFrame([{}])
 
+get_parser = reqparse.RequestParser()
+get_parser.add_argument('q', dest='query', default='',
+                        type=str, required=True,
+                        choices=('', 'trips'),
+                        help='Type of query - "", "trips"')
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('action', type=str, required=True,
+                        choices=('get', 'return'),
+                        help='Type of action - "get" or "return". If "return", optionally provide drop-off station.')
+put_parser.add_argument('station',
+                        type=int, required=False,
+                        help='Optional return station id.')
+
 def bike_exists(bike_id):
     if bike_id not in BIKES.loc['id']:
         abort(404, message="Bike ID: {} does not exist!".format(bike_id))
